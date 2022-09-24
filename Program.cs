51c1aaa41882883c45fa173f3eca507c8db838f8
Ahmed-Imad <ahmedfattal82@gmail.com>
2022-09-24 12:32:37 +0400

    Search functionality added to project and Identity framework added and updated database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,18 +13,25 @@ builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCa
 builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddJsonOptions( options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 {
     options.UseSqlServer(
         builder.Configuration ["ConnectionStrings:BethanysPieShopDbContextConnection"]);
 });
+builder.Services.AddControllers(); 
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseSession();
+app.UseAuthentication();
+//app.UseAuthorization();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
